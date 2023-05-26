472e2e24391f34b7eebcac898c13f8e646d952d4
Rio Del Pamaong <riodelpamaong@Rio-Del-Pamaong-MBP16.local>
2023-05-26 12:29:44 +0800

    Refactor Browse Page styling using Tailwind CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     extend: {
       colors: {
         blanchedalmond: '#FFEBCD',
-        peach: '#faf1ed'
+        peach: '#faf1ed',
+        'badge-budet-hotel': '#f2b203',
+        'badge-casino-hotel': '#3366cc',
+        'badge-spa-hotel': '#ff66cc',
+        'mystic-shadow': 'rgba(0, 0, 0, 0.6)',
+        'semi-transparent-black':'rgba(0, 0, 0, 0.6) calc(100% - 24px)',
+        'transparent': 'rgba(0, 0, 0, 0) 100%'
       },
       gridAutoColumns: {
         '100': '100%',
